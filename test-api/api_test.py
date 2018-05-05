76ff037d2f0d9f805dfb1d6d3a780aef2f8d183a
AddaKowalczyk8 <AddaKowalczyk8@users.noreply.github.com>
2018-05-05 11:54:45 +0200

    dodanie bibliotek w test_requirements, dodanie w scipt w travis.yml

--- test-api/api_test.py
+++ test-api/api_test.py
@@ -7,6 +7,7 @@ def main():
     """
     try:
         r = requests.get("http://127.0.0.1:5000?output=json")
+        print(r.text)
     except requests.exceptions.RequestException as e:
         print(e)
         exit(1)
