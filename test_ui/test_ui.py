67e6232f970e60a27a13e1ac2c069ac9bdad441c
AddaKowalczyk8 <AddaKowalczyk8@users.noreply.github.com>
2018-05-21 22:45:58 +0200

    Poprawa testow ui - poprawa wywolania chrome

--- test_ui/test_ui.py
+++ test_ui/test_ui.py
@@ -4,23 +4,16 @@ import unittest
 import time
 import pytest
 
-SELENIUM_WEBDRIVERS = {
-    'default': {
-        'callable': webdriver.Chrome,
-        'args': (),
-        'kwargs': {},
-    },
-    'firefox': {
-        'callable': webdriver.Firefox,
-        'args': (),
-        'kwargs': {},
-    },
-}
-
 @pytest.mark.uitest
 class TestFormater(unittest.TestCase):
     def test_plain_lowercase(self):
-        driver = webdriver.Chrome()
+        options = webdriver.ChromeOptions()
+        #All the arguments added for chromium to work on selenium
+        options.add_argument("--no-sandbox") #This make Chromium reachable
+        options.add_argument("--no-default-browser-check") #Overrides default choices
+        options.add_argument("--no-first-run")
+        options.add_argument("--disable-default-apps")
+        driver = webdriver.Chrome('/home/travis/virtualenv/python2.7.14/bin/chromedriver',chrome_options=options')
         driver.get("http://127.0.0.1:5000/ui")
         link = driver.find_element_by_xpath("/html/body/div/div/a")
         link.click()
